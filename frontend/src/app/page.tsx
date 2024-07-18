68d81b0cc74076bab03b4804b6061a82a6681ea8
DaviBCSouza <contato@davibrenno.com>
2024-07-18 00:26:01 -0400

    :recycle: [REFACTOR]: Alterando o Bootstrap pelo MUI

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,15 @@
+import { Box, Typography } from "@mui/material";
+
 export default function Home() {
   return (
-    <div className="d-flex flex-column align-items-center justify-content-center">
-      <h1>Project +</h1>
-      <p>Hello, Project +!</p>
-    </div>
+    <Box
+      alignItems="center"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+    >
+      <Typography variant="h3">Project +</Typography>
+      <Typography component="p">Hello, Project +</Typography>
+    </Box>
   );
 }
